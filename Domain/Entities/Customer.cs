5c2d51c9051bd65c78b4661d401607562c740472
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-11-09 11:21:59 -0500

    feat: :tada: Initial project
    Configuration initial
    
    @duvanrg

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Entities;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Idcustomer { get; set; } = null!;
+
+    public int IdTipoPersonaFk { get; set; }
+
+    public DateOnly DateRegister { get; set; }
+
+    public int IdcityFk { get; set; }
+
+    public virtual PersonType IdTipoPersonaFkNavigation { get; set; } = null!;
+
+    public virtual City IdcityFkNavigation { get; set; } = null!;
+}
